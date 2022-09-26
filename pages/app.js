8e4f183ae2b3408a214574fcf786f45b7c78cade
Markus <git@catchall.ma-ni.org>
2022-09-26 13:08:48 +0000

    end

---
+++ pages/app.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function App() {
+	return (
+		<div className="container">
+			<Head>
+				<title>Create Next App</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+			<main>
+				<h1 className="title">Welcome to App!!</h1>
+			</main>
+		</div>
+	);
+}
